79b62747f905732bff460de3df835393ad7ad70c
melyman95 <mattlyman12@gmail.com>
2021-07-23 22:51:50 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Models_And_Model_Binding.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models_And_Model_Binding.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+    }
+}
